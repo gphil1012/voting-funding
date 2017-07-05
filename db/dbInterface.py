f266fa6b3a92a657081835fff8d17e03bcec9d39
Chris Palmer <capalmer1013@gmail.com>
2017-07-04 22:12:38 -0400

    adding more tests and interfaces

--- db/dbInterface.py
+++ db/dbInterface.py
@@ -2,14 +2,32 @@
 These are the interfaces that will be used
 in order to interact with the database
 """
+import sqlite3
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
 class OutgoingInterface(object):
     """
     This interface will be used by the server
     in order to generate the data to be used
     by the front end to display information to the user
     """
-    def __init__(self, dbFilename):
-        pass
+    def __init__(self, dbFilename, dbDriver=sqlite3):
+        self.dbFilename = dbFilename
+        self.dbDriver = dbDriver
+
+    def openConnection(self):
+        conn = self.dbDriver.connect(self.dbFilename)
+        conn.row_factory = dict_factory
+        return conn
+
+    def closeConnection(self, conn):
+        conn.commit()
+        conn.close()
 
     def getAllCongressMembers(self):
         """
@@ -18,10 +36,22 @@ class OutgoingInterface(object):
             "name", "houseRep", "ID", "DOB"
         }]
         """
-        pass
+        c = self.openConnection()
+        cur = c.cursor().execute("select * from congress")
+        value = cur.fetchall()
+        self.closeConnection(c)
+        return value
 
     def getCongressMemberById(self, searchID):
-        pass
+        """
+        returns a dict of a congress member of the form
+        {"name", "houseRep", "ID", "DOB"}
+        """
+        c = self.openConnection()
+        cur = c.cursor().execute("select * from congress where ID=?", (searchID,))
+        value = cur.fetchone()
+        self.closeConnection(c)
+        return value
     
     def getAllDonations(self):
         pass
@@ -52,14 +82,32 @@ class IncomingInterface(OutgoingInterface):
     This interface will be used by the webscraper
     in order to update or add any new information to the database
     """
-    def __init__(self, dbFilename):
-        super(IncomingInterface, self).__init__(dbFilename)
+    def __init__(self, dbFilename, dbDriver=sqlite3):
+        super(IncomingInterface, self).__init__(dbFilename, dbDriver)
+        self.dbDriver = dbDriver
+        self.dbFilename = dbFilename
 
-    def addCongressMember(self):
-        pass
+    def addCongressMember(self, first_name, last_name, houseRep, dob, email=""):
+        """
+        first_name - string
+        last_name - string
+        houseRep - boolean
+        dob - string dd-mm-yyyy
+        """
+        c = self.openConnection()
+        c.cursor().execute("INSERT INTO congress (first_name, last_name, house_rep, dob, email) VALUES (?,?,?,?,?)", (first_name, last_name, houseRep, dob, email))
+        self.closeConnection(c)
 
-    def addDonation(self):
-        pass
+    def addDonation(self, congress_ID, org_ID, donation_amount, year):
+        """
+        congress_ID - int foreign key
+        org_ID - int foreign key
+        donation_amount - float $$$
+        year - int
+        """
+        c = self.openConnection()
+        c.cursor().execute("INSERT INTO funding (congress_ID, organization_ID, donation_amount, year) VALUES (?,?,?,?)", (congress_ID, org_ID, donation_amount, year))
+        self.closeConnection(c)
 
     def addOrganization(self):
         pass
--- tests/test_dbInterface.py
+++ tests/test_dbInterface.py
@@ -1,10 +1,62 @@
+"""
+test suite for the dbInterface module
+"""
+import sys
 import unittest
 import hypothesis
+from hypothesis import given, strategies as st
+sys.path.append("../db/")
+import dbInterface
+from datetime import date
+import random
+
+testDb = "../db/zionMainframe.db"
+
+class TestGeneral(unittest.TestCase):
+    def setup(self):
+        dbOut = dbInterface.OutgoingInterface(testDb)
+        dbIn = dbInterface.IncomingInterface(testDb)
+
+    def test_Build(self):
+        print "seems to build"
 
 class TestCongressTable(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.inDb = dbInterface.IncomingInterface(testDb)
+        self.outDb = dbInterface.OutgoingInterface(testDb)
+    
+    @given(name=st.text(), houseRep=st.booleans(), email=st.text())
+    def test_addingCongressMember(self, name, houseRep, email):
+        dob = date(1969, 6, 9).strftime("%x")
+        self.inDb.addCongressMember(name, name, houseRep, dob, email)
+
+    def test_getAllCongressMembers(self):
+        a = self.inDb.getAllCongressMembers()
+    
+    def test_getCongressMemberById(self):
+        a = random.choice(self.inDb.getAllCongressMembers())
+        self.inDb.getCongressMemberById(a['ID'])
 
 class TestFundingTable(unittest.TestCase):
+    def setUp(self):
+        self.inDb = dbInterface.IncomingInterface(testDb)
+        self.outDb = dbInterface.OutgoingInterface(testDb)
+    
+    def test_addDonation(self):
+        pass
+
+    def test_getAllDonations(self):
+        pass
+    
+    def test_getDonationsByOrganization(self):
+        pass
+
+    def test_getDonationsByCongressId(self):
+        pass
+    
+    def test_getDonationById(self):
+        pass
+
     pass
 
 class TestVotesTable(unittest.TestCase):
